2a84d937c76b16e4777f8d5744e3e941cdd1a260
Eduardo Rodrigues <eduxdudu@icloud.com>
2024-05-07 10:40:59 -0300

    Primeiro commit

--- ContaBanco/src/ContaTerminal.java
+++ ContaBanco/src/ContaTerminal.java
@@ -1,5 +1,42 @@
-public class ContaTerminal {
-    public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
+import java.util.Locale;
+import java.util.Scanner;
+
+public class ContaTerminal 
+{
+    public static void main(String[] args) throws Exception 
+    {
+        //TODO: Conhecer e importar a classe Scanner
+
+        //TODO: Exibir as mensagens para nosso usuário
+
+        //TODO: Obter pela classe Scanner os valores digitados no terminal
+
+        //TODO: Exibir a mensagem conta criada
+
+        Scanner scanner = new Scanner(System.in);
+        
+        double saldo = 50;
+
+        System.out.println("Olá! Bem vindo a criação de conta do nosso banco!");
+        System.out.println("Crie a sua conta hoje e ganhe R$ 50,00 como saldo inicial!");
+
+        System.out.println("Por favor, digite o seu nome:");
+        String nome = scanner.next();
+
+        System.out.println("Agora digite o seu sobrenome:");
+        String sobrenome = scanner.next();
+
+        System.out.println("Digite o número de sua agência de preferência:");
+        System.out.println("776 CENTRO");
+        System.out.println("568 ZONA LESTE");
+        System.out.println("443 ZONA SUL");
+        int agencia = scanner.nextInt();
+
+        System.out.println("Agora digite o número desejado para sua nova conta:");
+        int conta = scanner.nextInt();
+
+
+        System.out.println("Bem vindo " + nome + " " + sobrenome + ", obrigado por criar uma conta em nosso banco!");
+        System.out.println("Sua agência é " + agencia + ", conta " + conta + " e seu saldo R$ " + saldo + " já está disponível para saque!");        
     }
 }
